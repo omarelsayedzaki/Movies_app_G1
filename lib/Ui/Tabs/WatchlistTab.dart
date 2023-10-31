0ad6e07344f4f75d9337cc0dd94fd910d54d8ee6
omarelsayedzaki <omar.elsayed.zaki@gmail.com>
2023-10-31 19:24:24 +0200

    -add BrowseTab - add WatchlistTab -add WatchListItem

--- lib/Ui/Tabs/WatchlistTab.dart
+++ lib/Ui/Tabs/WatchlistTab.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../watchListItem/WatchListItem.dart';
+
 class WatchlistTab extends StatelessWidget {
   const WatchlistTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Watchlist",
+            style: TextStyle(fontSize: 22),
+          ),
+          Expanded(
+            child: ListView(children: [
+              WatchListItem(),
+              WatchListItem(),
+              WatchListItem(),
+              WatchListItem(),
+              WatchListItem(),
+              WatchListItem(),
+              WatchListItem(),
+              WatchListItem(),
+              WatchListItem(),
+            ]),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/Ui/Tabs/homeScreenTab.dart
+++ lib/Ui/Tabs/homeScreenTab.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../DetailsScreen.dart';
 import '../recomendrdWidget/recomendrdWidget.dart';
 
